61c15ca9fa159155db4c1bd04680dc91874d5b16
Aman kuril <kurilaman21@gmail.com>
2025-04-22 16:38:03 +0530

    2 questions done

---
+++ leetcode_744.java
@@ -0,0 +1,21 @@
+public class leetcode_744 {
+    public static void main(String[] args) {
+        char[] letters = { 'c', 'f', 'j' };
+        char target = 'a';
+        System.out.println(nextGreatestLetter(letters, target)); // Output: c
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return letters[left % letters.length]; // Wrap around to the start of the array
+    }
+}
